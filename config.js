bb05a2f4a88aa631b5d435029555eb16fb4db7d5
A Language <52973315+alng-org@users.noreply.github.com>
2025-12-03 06:19:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+function statement(){
+    return  [
+        "White is White, Not Black",
+        "What you found is what you think"
+    ];
+}
+function position(){
+    //background position
+    //url of image is ./<position>.jpg
+    //see more at function background in ./index.html
+    return [
+        "bottom left",
+        "bottom",
+        "bottom right",
+        "center",
+        "top right",
+        "top",
+        "top left"
+    ];
+}
